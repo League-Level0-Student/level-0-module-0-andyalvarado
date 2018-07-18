21b003bbeba665ced5f002ac9e0ab97c04298e32
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-17 17:53:53 -0700

    greeter

--- src/greeter.java
+++ src/greeter.java
@@ -1,6 +1,30 @@
+import javax.swing.JOptionPane;
 
 public class greeter {
 public static void main(String[] args) {
+	String name = JOptionPane.showInputDialog("Enter your name");
+	JOptionPane.showMessageDialog(null,"HELLO  " + name);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
 }
